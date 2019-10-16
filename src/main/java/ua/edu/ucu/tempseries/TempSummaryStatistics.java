26431b492e1f3cb9498dd94dabc0ee578dacf9cc
MarianDubei <dubei@ucu.edu.ua>
2019-10-16 21:43:43 +0300

    Updated TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,5 +1,15 @@
 package ua.edu.ucu.tempseries;
 
-public class TempSummaryStatistics {
-    
+public final class TempSummaryStatistics {
+    double avgTemp;
+    double devTemp;
+    double minTemp;
+    double maxTemp;
+
+    public TempSummaryStatistics(double avg, double dev, double min, double max){
+        this.avgTemp = avg;
+        this.devTemp = dev;
+        this.minTemp = min;
+        this.maxTemp = max;
+    }
 }
